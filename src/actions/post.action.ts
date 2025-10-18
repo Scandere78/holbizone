d8130ea960afc4b98c938eadc7073ff92cd41e7b
ScandereT78 <scanderetejpro@gmail.com>
2025-10-18 20:05:46 +0200

    fix problème tx

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -3,6 +3,7 @@
 import prisma from "@/lib/prisma";
 import { getDbUserId } from "./user.action";
 import { revalidatePath } from "next/cache";
+import { Prisma } from "@prisma/client";
 
 export async function createPost(content: string, image: string) {
   try {
@@ -156,7 +157,7 @@ export async function createComment(postId: string, content: string) {
     if (!post) throw new Error("Post not found");
 
     // Create comment and notification in a transaction
-    const [comment] = await prisma.$transaction(async (tx) => {
+    const [comment] = await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
       // Create comment first
       const newComment = await tx.comment.create({
         data: {
